b5ad52a18e73a4f8406f1e3318b1ba27d03858f7
Talha Ishtiaque <talha@Talhas-MacBook-Pro.local>
2019-04-21 14:47:06 -0700

    habrproxy

--- habraproxy.py
+++ habraproxy.py
@@ -6,7 +6,7 @@ from bottle import route, run, response, request
 from bs4 import BeautifulSoup, Comment, Doctype
 
 
-SITE_URL = 'https://habr.com/ru/company/yandex/blog/258673/'
+SITE_URL = 'https://habrahabr.ru'
 HOST = 'localhost'
 PORT = 8088
 
@@ -18,8 +18,6 @@ def main():
 @route('/')
 @route('/<path:path>')
 def index(path=None):
-    print request.url
-    print request.query.url
     if request.query.url:
         url = request.query.url
     else:
@@ -36,69 +34,24 @@ def index(path=None):
 
 def parse(content):
     soup = BeautifulSoup(content, "lxml")
-
-    # for row in soup.find_all('div'):
-        #print row.text
-        #print type(row.text)
-        # for word in row.text:
-        #     print word
-    # for a in soup('a'):
-    #     href = a.get('href')
-    #     if href:
-    #         a['href'] = href.replace(SITE_URL, '')
-    #
     for a in soup.find_all('a', href=True):
         if 'https://habr.com' in a['href']:
             a['href'] = 'http://localhost:8088/?url='+a['href']
 
     for tag in soup.find_all(string=lambda s: not isinstance(s, (Comment, Doctype))):
-    # for tag in soup.find_all('div'):
-    #     print(parent.name)
         if tag.parent.name not in ('style', 'script', 'noscript', 'head', '[document]'):
-        #     continue
-        # print tag.text
             text = tag.string.strip()
-        # print tag.text
-        #text = tag.text
-            if text is not None:
-                #print add_trademark(text)
-                try:
-
-                    regex = re.compile(r'\b\w{6}\b', re.UNICODE )
-                    # myfile =  'foo"s bar'
-                    # myfile2 = regex.sub(lambda m: m.group().replace(r'\b\w{6}\b',"1",1), myfile)
-
-                    newtext = regex.sub(lambda m: m.group()+unicode('™', "utf-8"), text)
-                    tag.string.replaceWith(newtext)
-                    # matches = re.findall(r'\b\w{6}\b',text, re.MULTILINE)
-                    # # print matches
-                    # if len(matches)>0:
-                    #     # print len(matches)
-                    #     # print matches
-                    #     newtext = re.sub(r'\b\w{6}\b', 'abc' , text)
-                    #     print(newtext)
-                    #     tag.string.replaceWith(newtext)
-                    #     # print(text2)
-                except Exception as e:
-                    print e
+            if text:
+                tag.string.replace_with(add_trademark(text))
+        else:
+            continue
     return soup.prettify().encode('utf-8')
 
 
 def add_trademark(text):
-    # regex = re.compile(u'(?<=\b)(?<!-)(\w{6})(?!-)(?=\b)', re.UNICODE)
-    # print regex.sub(u'\\1™', text)
-    # return regex.sub(u'\\1™', text)
-    # regex = re.compile('\b\w{6}\b', re.UNICODE)
-    # print re.sub('\b\w{6}\b', '1™' , text)
-    # return re.sub('\b\w{6}\b', 'abc' , text)
-    matches = re.findall(r'\b\w{6}\b',text, re.MULTILINE)
-    # print matches
-    if len(matches)>0:
-        print len(matches)
-        print matches
-    return re.sub('\b\w{6}\b', 'abc' , text)
+    regex = re.compile(r'\b\w{6}\b', re.UNICODE )
+    return regex.sub(lambda m: m.group()+unicode('™', "utf-8"), text)
 
-# def parse_links():
 
 if __name__ == '__main__':
     main()
